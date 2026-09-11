9c72d2b1c0cb452df4dc3c8fb11c619a42342d51
polledonievesm <polledonievesm@gmail.com>
2026-09-10 21:23:30 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 window.CONTROL_PAGOS_CONFIG = Object.freeze({
-  apiUrl: 'https://script.google.com/macros/s/AKfycbwiuneKjJPqVDlfhaY9Zy3FjuE0VhOQfnaMi2DvUjAK_QSp5EnVjjZKgyoInA9d7TCa9A/exec'
+  apiUrl: 'https://script.google.com/macros/s/AKfycbymtttzfh4RAs5tmL4Bccmm6caoKMUpDXNM-gCAit_ozayZy6PsDqxbNWMT2znc2oqg/exec'
 });
